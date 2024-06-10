607a284716dedb68f8371296e65dad235ffe2fd9
Enzo LEMAITRE <lemaitreenzo05@gmail.com>
2024-06-10 10:23:49 +0200

    Update FiderApi.credentials.ts

---
+++ credentials/FiderApi.credentials.ts
@@ -0,0 +1,45 @@
+import {
+	IAuthenticateGeneric,
+	ICredentialTestRequest,
+	ICredentialType,
+	INodeProperties,
+} from 'n8n-workflow';
+
+export class FiderApi implements ICredentialType {
+	name = 'fiderApi';
+	displayName = 'Fider API';
+	documentationUrl = 'https://fider.io/docs/api#authentication';
+	properties: INodeProperties[] = [
+		{
+			displayName: 'Domain',
+			name: 'domain',
+			type: 'string',
+			default: 'https://localhost',
+		},
+		{
+			displayName: 'Token',
+			name: 'token',
+			type: 'string',
+			typeOptions: {
+				password: true,
+			},
+			default: '',
+		},
+	];
+	authenticate: IAuthenticateGeneric = {
+		type: 'generic',
+		properties: {
+			headers: {
+				Authorization: '={{"Bearer " + $credentials.authToken}}',
+			},
+		},
+	};
+
+	// The block below tells how this credential can be tested
+	test: ICredentialTestRequest = {
+		request: {
+			baseURL: '={{$credentials?.domain}}',
+			url: '/bearer',
+		},
+	};
+}
--- nodes/FiderNode/FiderNode.node.ts
+++ nodes/FiderNode/FiderNode.node.ts
@@ -1,4 +1,5 @@
 import {
+	IDataObject,
 	IExecuteFunctions,
 	INodeExecutionData,
 	INodeType,
@@ -6,71 +7,264 @@ import {
 	NodeOperationError,
 } from 'n8n-workflow';
 
+import * as GenericFunctions from './GenericFunctions';
+import { fiderNodeFields } from './fiderNodeDescription';
+
 export class FiderNode implements INodeType {
 	description: INodeTypeDescription = {
 		displayName: 'Fider',
 		name: 'fiderNode',
+		icon: 'file:fider.svg', // Remplacer par une icône SVG
 		group: ['transform'],
 		version: 1,
-		description: 'Fider node',
+		description: 'Interact with Fider API',
 		defaults: {
-			name: 'Fider',
+			name: 'fider',
 		},
+		subtitle: '={{ $parameter["operation"] + ": " + $parameter["resource"] }}',
 		inputs: ['main'],
 		outputs: ['main'],
-		properties: [
-			// Node properties which the user gets displayed and
-			// can change on the node.
+		credentials: [
 			{
-				displayName: 'String',
-				name: 'myString',
-				type: 'string',
-				default: '',
-				placeholder: 'Placeholder value',
-				description: 'The description text',
+				name: 'fiderApi',
+				required: true,
 			},
+		],
+		properties: [
+			{
+				displayName: 'Resource',
+				name: 'resource',
+				type: 'options',
+				noDataExpression: true, // Ajouté
+				options: [
+					{
+						name: 'Post',
+						value: 'post',
+					},
+					{
+						name: 'Comment',
+						value: 'comment',
+					},
+					{
+						name: 'User',
+						value: 'user',
+					},
+					{
+						name: 'Sample',
+						value: 'sample',
+					},
+				],
+				default: 'post',
+			},
+			{
+				displayName: 'Operation',
+				name: 'operation',
+				type: 'options',
+				noDataExpression: true,
+				displayOptions: {
+					show: {
+						resource: ['post'],
+					},
+				},
+				options: [
+					{
+						name: 'Create',
+						value: 'create',
+						action: 'Create',
+					},
+					{
+						name: 'Delete',
+						value: 'delete',
+						action: 'Delete',
+					},
+					{
+						name: 'Edit',
+						value: 'edit',
+						action: 'Edit',
+					},
+					{
+						name: 'Get',
+						value: 'get',
+						action: 'Get',
+					},
+					{
+						name: 'Vote',
+						value: 'vote',
+						action: 'Vote',
+					},
+				],
+				default: 'create',
+			},
+			{
+				displayName: 'Operation',
+				name: 'operation',
+				type: 'options',
+				noDataExpression: true,
+				displayOptions: {
+					show: {
+						resource: ['comment'],
+					},
+				},
+				options: [
+					{
+						name: 'Create',
+						value: 'create',
+						action: 'Create',
+					},
+					{
+						name: 'Delete',
+						value: 'delete',
+						action: 'Delete',
+					},
+					{
+						name: 'Edit',
+						value: 'edit',
+						action: 'Edit',
+					},
+					{
+						name: 'Get',
+						value: 'get',
+						action: 'Get',
+					},
+				],
+				default: 'create',
+			},
+			{
+				displayName: 'Operation',
+				name: 'operation',
+				type: 'options',
+				noDataExpression: true,
+				displayOptions: {
+					show: {
+						resource: ['user'],
+					},
+				},
+				options: [
+					{
+						name: 'Create',
+						value: 'create',
+						action: 'Create',
+					},
+					{
+						name: 'Get',
+						value: 'get',
+						action: 'Get',
+					},
+				],
+				default: 'create',
+			},
+			{
+				displayName: 'Operation',
+				name: 'operation',
+				type: 'options',
+				noDataExpression: true,
+				displayOptions: {
+					show: {
+						resource: ['sample'],
+					},
+				},
+				options: [
+					{
+						name: 'Send',
+						value: 'send',
+						action: 'Send',
+					},
+				],
+				default: 'send',
+			},
+			...fiderNodeFields,
 		],
 	};
 
-	// The function below is responsible for actually doing whatever this node
-	// is supposed to do. In this case, we're just appending the `myString` property
-	// with whatever the user has entered.
-	// You can make async calls and use `await`.
 	async execute(this: IExecuteFunctions): Promise<INodeExecutionData[][]> {
 		const items = this.getInputData();
+		const returnData: INodeExecutionData[] = [];
 
-		let item: INodeExecutionData;
-		let myString: string;
+		for (let i = 0; i < items.length; i++) {
+			const resource = this.getNodeParameter('resource', i) as string;
+			const operation = this.getNodeParameter('operation', i) as string;
 
-		// Iterates over all input items and add the key "myString" with the
-		// value the parameter "myString" resolves to.
-		// (This could be a different value for each item in case it contains an expression)
-		for (let itemIndex = 0; itemIndex < items.length; itemIndex++) {
 			try {
-				myString = this.getNodeParameter('myString', itemIndex, '') as string;
-				item = items[itemIndex];
-
-				item.json['myString'] = myString;
-			} catch (error) {
-				// This node should never fail but we want to showcase how
-				// to handle errors.
-				if (this.continueOnFail()) {
-					items.push({ json: this.getInputData(itemIndex)[0].json, error, pairedItem: itemIndex });
-				} else {
-					// Adding `itemIndex` allows other workflows to handle this error
-					if (error.context) {
-						// If the error thrown already contains the context property,
-						// only append the itemIndex
-						error.context.itemIndex = itemIndex;
-						throw error;
+				if (resource === 'post') {
+					if (operation === 'create') {
+						const title = this.getNodeParameter('title', i) as string;
+						const description = this.getNodeParameter('description', i) as string;
+						const responseData = await GenericFunctions.createNewPost.call(this, title, description);
+						returnData.push({ json: responseData });
 					}
-					throw new NodeOperationError(this.getNode(), error, {
-						itemIndex,
-					});
+					if (operation === 'get') {
+						const responseData = await GenericFunctions.getPosts.call(this);
+						returnData.push({ json: responseData });
+					}
+					if (operation === 'edit') {
+						const postId = this.getNodeParameter('postId', i) as number;
+						const updatedData = this.getNodeParameter('updateFields', i) as IDataObject;
+						const responseData = await GenericFunctions.editPost.call(this, postId, updatedData);
+						returnData.push({ json: responseData });
+					}
+					if (operation === 'delete') {
+						const postId = this.getNodeParameter('postId', i) as number;
+						await GenericFunctions.deletePost.call(this, postId);
+						returnData.push({ json: { success: true } });
+					}
+					if (operation === 'vote') {
+						const postId = this.getNodeParameter('postId', i) as number;
+						await GenericFunctions.votePost.call(this, postId);
+						returnData.push({ json: { success: true } });
+					}
 				}
+				if (resource === 'comment') {
+					if (operation === 'create') {
+						const postId = this.getNodeParameter('postId', i) as number;
+						const content = this.getNodeParameter('content', i) as string;
+						const responseData = await GenericFunctions.addComment.call(this, postId, content);
+						returnData.push({ json: responseData });
+					}
+					if (operation === 'get') {
+						const postID = this.getNodeParameter('postId', i) as number;
+						const responseData = await GenericFunctions.getComments.call(this, postID);
+						returnData.push({ json: responseData });
+					}
+					if (operation === 'edit') {
+						const postId = this.getNodeParameter('postId', i) as number;
+						const commentId = this.getNodeParameter('postId', i) as number;
+						const updatedData = this.getNodeParameter('updateFields', i) as IDataObject;
+						const responseData = await GenericFunctions.editComment.call(this, postId, commentId, updatedData);
+						returnData.push({ json: responseData });
+					}
+					if (operation === 'delete') {
+						const postId = this.getNodeParameter('postId', i) as number;
+						const commentId = this.getNodeParameter('postId', i) as number;
+						await GenericFunctions.deleteComment.call(this, postId, commentId);
+						returnData.push({ json: { success: true } });
+					}
+				}
+				if (resource === 'user') {
+					if (operation === 'create') {
+						const name = this.getNodeParameter('userName', i) as string;
+						const email = this.getNodeParameter('email', i) as string;
+						const responseData = await GenericFunctions.createUser.call(this, name, email);
+						returnData.push({ json: responseData });
+					}
+					if (operation === 'get') {
+						const responseData = await GenericFunctions.getUsers.call(this);
+						returnData.push({ json: responseData });
+					}
+				}
+				if (resource === 'sample') {
+					const subject = this.getNodeParameter('subject', i) as string;
+					const message = this.getNodeParameter('message', i) as string;
+					const responseData = await GenericFunctions.sendASample.call(this, subject, message);
+					returnData.push({ json: responseData });
+				}
+			} catch (error) {
+				if (this.continueOnFail()) {
+					returnData.push({ json: { error: error.message } });
+					continue;
+				}
+				throw new NodeOperationError(this.getNode(), error);
 			}
 		}
-
-		return this.prepareOutputData(items);
+		return [returnData];
 	}
 }
